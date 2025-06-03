d7776be95e72be6a476b08e6fd9feadbf6aa2515
gaengmin <momo5736@naver.com>
2025-06-03 09:23:15 +0900

    fix : swagger 문서화 추가

--- src/main/java/tableOrder/category/controller/CategoriesController.java
+++ src/main/java/tableOrder/category/controller/CategoriesController.java
@@ -1,5 +1,6 @@
 package tableOrder.category.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -27,11 +28,11 @@ public class CategoriesController {
     private final CategoriesService categoriesService;
     private final MenuService menuService;
 
-    /**
-     * TODO : 고객용
-     * 판매 중단 : 메뉴는 맨 밑으로 내리고
-     * 판매용 메뉴 위주가 먼저 보이게
-     * 카테고리 안에 존재하는 메뉴를 보기 **/
+
+    @Operation(
+            summary = "고객용, 특정 카테고리의 메뉴 목록 조회",
+            description = "storeNo와 categoriesNo를 받아 해당 카테고리의 메뉴 목록을 반환합니다. 판매중단 메뉴는 하단에 정렬."
+    )
     @GetMapping("/categories/{categoriesNo}/stores/{storeNo}/menu")
     public ResponseEntity<List<ResponseCategoryDto.ResponseListMenuDto>> getMenusByCategoriesNo(@PathVariable int categoriesNo, @PathVariable int storeNo) {
 
@@ -44,6 +45,10 @@ public class CategoriesController {
     /**
      * 카테고리 Position 수정 API
      */
+    @Operation(
+            summary = "카테고리 순서(position) 변경",
+            description = "관리자가 카테고리의 순서를 변경합니다. body에 순서대로 배치할 카테고리 ID 리스트(Long) 전달"
+    )
     @PreAuthorize("hasAuthority('ADMIN')")
     @SecurityRequirement(name = "Access")
     @PatchMapping("/categories/rePosition")
@@ -58,6 +63,10 @@ public class CategoriesController {
      * - 성공 시 200 OK와 성공 메시지 반환
      * - 실패(카테고리 없음 등) 시 400 Bad Request와 에러 메시지 반환
      */
+    @Operation(
+            summary = "카테고리명 수정",
+            description = "카테고리 번호와 새로운 이름을 받아 관리자가 카테고리명을 변경합니다."
+    )
     @PatchMapping("/categories/{categoriesNo}/updateName")
     @SecurityRequirement(name = "Access")
     @PreAuthorize("hasAuthority('ADMIN')")
@@ -74,6 +83,10 @@ public class CategoriesController {
      * - 성공 시 201(Created)과 메시지 반환
      * - 유효하지 않은 데이터(IllegalArgumentException)는 400(Bad Request)로 처리
      */
+    @Operation(
+            summary = "카테고리 등록",
+            description = "관리자가 새로운 카테고리를 등록합니다."
+    )
     @PostMapping("/categories")
     @SecurityRequirement(name = "Access")
     @PreAuthorize("hasAuthority('ADMIN')")
@@ -91,6 +104,10 @@ public class CategoriesController {
      * - 성공 시 200(OK)과 메시지 반환
      * - 유효하지 않은 데이터(IllegalArgumentException)는 400(Bad Request)로 처리
      */
+    @Operation(
+            summary = "카테고리(소프트) 삭제",
+            description = "관리자가 카테고리를 소프트 삭제합니다. (논리적 삭제)"
+    )
     @PatchMapping("/categories/{categoriesNo}/soft-delete")
     @SecurityRequirement(name = "Access")
     @PreAuthorize("hasAuthority('ADMIN')")
